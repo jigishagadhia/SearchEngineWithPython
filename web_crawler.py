2ecc1ff089c6259df85286b6673d0cd2e6999170
Jigisha <jigishagadhia007@gmail.com>
2020-07-20 16:19:52 +0530

    Added get_page(url) and get_a_link(page)

--- web_crawler.py
+++ web_crawler.py
@@ -1,2 +1,21 @@
-print("This is a web Crawler!!")
-print("This is a crazy Web Crawler")
+import urllib.request
+def get_page(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+        return html
+    except:
+        return ""
+
+def get_a_link(page):
+	page=str(page)
+	start_link=page.find("<a href=")
+	start_quote=page.find('"',start_link)
+	end_quote=page.find('"',start_quote+1)
+	url=page[start_quote+1:end_quote]
+	page=page[end_quote:]
+	return url
+
+seed='https://www.wikipedia.org/'
+page=get_page(seed)
+print(get_a_link(page))
