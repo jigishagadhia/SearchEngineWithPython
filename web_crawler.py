4e9ad52f85995a6885c8ea8b50177db8e78be82c
Jigisha <jigishagadhia007@gmail.com>
2020-07-20 16:37:39 +0530

    Added get_all_links(page) and modified get_a_link(page)

--- web_crawler.py
+++ web_crawler.py
@@ -10,12 +10,24 @@ def get_page(url):
 def get_a_link(page):
 	page=str(page)
 	start_link=page.find("<a href=")
+	if start_link==-1:
+		return None,0
 	start_quote=page.find('"',start_link)
 	end_quote=page.find('"',start_quote+1)
 	url=page[start_quote+1:end_quote]
-	page=page[end_quote:]
-	return url
+	return url,end_quote
+
+def get_all_links(page):
+	all_links=[]
+	while True:
+		link,endpos=get_a_link(page)
+		if link:
+			all_links.append(link)
+			page=page[endpos+1:]
+		else:
+			break
+	return all_links
 
 seed='https://www.wikipedia.org/'
 page=get_page(seed)
-print(get_a_link(page))
+print(get_all_links(page))
